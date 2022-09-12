81adb93c29f0d40eb256250a2f654526500c40ab
pgpablodev <povarg.pablo@gmail.com>
2022-09-12 18:23:42 +0200

    Changes on appearence. Restart app button added on results screen.

--- app/src/main/java/com/pablopg/caloriecalculator/MainActivity.kt
+++ app/src/main/java/com/pablopg/caloriecalculator/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.pablopg.caloriecalculator
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.RadioButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.result_screen.*
 import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.setTitle("Calculadora de calorías")
         setContentView(R.layout.activity_main)
 
+        inicio()
+    }
+
+    private fun inicio(){
         btnCalc.setOnClickListener{
             val indSexo: Int = rgSexo.checkedRadioButtonId
             val edad: Int = etEdad.getText().toString().toInt()
@@ -27,28 +34,43 @@ class MainActivity : AppCompatActivity() {
                 && indObjetivo != -1
                 && indEntre != -1
             ){
-                val sexo: String = findViewById<RadioButton>(indSexo).text.toString()
-                val objetivo: Double = findViewById<RadioButton>(indObjetivo).text.toString().toDouble()
-                val entrenamientos: Double = findViewById<RadioButton>(indEntre).text.toString().toDouble()
-
-                val mb: Double
-                if(sexo.equals("Hombre"))
-                    mb = 10*peso+6.25*altura-5*edad+5
-                else if(sexo.equals("Mujer"))
-                    mb = 10*peso+6.25*altura-5*edad-161
-                else
-                    mb = 0.0
-
-                val calMantenimiento: Double = mb*entrenamientos
-
-                val resFinal: Double = calMantenimiento*objetivo
-
-                setContentView(R.layout.result_screen)
-
-                tvMetBasal.setText("Metabolismo basal\n         " +(mb*100).roundToInt()/100 +" kcal")
-                tvCObj.setText("Requerimiento objetivo\n            " +(resFinal*100).roundToInt()/100 +" kcal")
-                tvCMan.setText("Calorías de mantenimiento: " +calMantenimiento.roundToInt() +" kcal")
+                calculos(indSexo,edad,altura,peso,indObjetivo,indEntre)
+            }else{
+                Toast.makeText(this,"Rellene todos los campos",Toast.LENGTH_LONG).show() //LENGTH_LONG = 3.5 segundos
             }
+        }
+    }
+
+    private fun calculos(indSexo: Int, edad: Int, altura: Double, peso: Double, indObjetivo: Int, indEntre: Int){
+        val sexo: String = findViewById<RadioButton>(indSexo).text.toString()
+        val objetivo: Double = findViewById<RadioButton>(indObjetivo).text.toString().toDouble()
+        val entrenamientos: Double = findViewById<RadioButton>(indEntre).text.toString().toDouble()
+
+        val mb: Double
+        if(sexo.equals("Hombre"))
+            mb = 10*peso+6.25*altura-5*edad+5
+        else if(sexo.equals("Mujer"))
+            mb = 10*peso+6.25*altura-5*edad-161
+        else
+            mb = 0.0
+
+        val calMantenimiento: Double = mb*entrenamientos
+
+        val resFinal: Double = calMantenimiento*objetivo
+
+        setContentView(R.layout.result_screen)
+
+        tvMetBasal.setText("Metabolismo basal\n         " +(mb*100).roundToInt()/100 +" kcal")
+        tvCObj.setText("Requerimiento objetivo\n            " +(resFinal*100).roundToInt()/100 +" kcal")
+        tvCMan.setText("Calorías de mantenimiento: " +calMantenimiento.roundToInt() +" kcal")
+
+        botonReinicio()
+    }
+
+    private fun botonReinicio(){
+        ivReload.setOnClickListener{
+            finish()
+            startActivity(getIntent())
         }
     }
 }
